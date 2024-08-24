d269dd031e761984bba3bd0f3cb0ef3ba87ca306
marwa172 <marwa@bluskyint.com>
2024-08-24 12:14:11 +0300

    unary operator overloading

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace demo
 {
-    #region operator overloading
+    #region  operator overloading
     class Complex
     {
         public int? Real { get; set; }
@@ -25,14 +25,32 @@
                 Img = (left?.Img ?? 0) - (right?.Img ?? 0)
             };
         }
+        #region unary operator overloading
+        public static Complex operator ++(Complex c)
+        {
+            return new Complex
+            {
+                Real = (c?.Real ?? 0) +1,
+                Img = (c?.Img ?? 0) 
+            };
+        }
+        public static Complex operator --(Complex c)
+        {
+            return new Complex
+            {
+                Real = (c?.Real ?? 0) - 1,
+                Img = (c?.Img ?? 0)
+            };
+        }
+        #endregion
     }
     #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region operator overloading
-            Complex c01 = new Complex() { Real = 2, Img = 6 };
+            #region  operator overloading
+            Complex c01 = new Complex() { Real = 12, Img = 6 };
             Console.WriteLine(c01);
 
             Complex c02 = new Complex() { Img = 4, Real = 9 };
@@ -43,7 +61,13 @@
             Console.WriteLine(c03);
             c03 = c01 - c02;
             Console.WriteLine(c03);
+            #region unary operator overloading
+            c03++;
+            Console.WriteLine(c03);
+            c03--;
+            Console.WriteLine(c03);
             #endregion
+#endregion
         }
     }
 }
