a12afd05f22355329f555bad1a110f0624715a3b
marwa172 <marwa@bluskyint.com>
2024-08-31 11:32:32 +0300

    assignment5

---
+++ Assigmet05-oop/First Project/3dPoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigmet05_oop.First_Project
+{
+    public class _3dPoint
+    {
+        #region Properties
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        #endregion
+        #region Costructors
+        public _3dPoint():this(0,0,0)
+        {
+
+        }
+        public _3dPoint(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X},{Y},{Z})”.";
+        }
+
+    }
+}
--- Assigmet05-oop/Program.cs
+++ Assigmet05-oop/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace Assigmet05_oop
+﻿
+using Assigmet05_oop.First_Project;
+using System.Drawing;
+
+namespace Assigmet05_oop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            _3dPoint[] Points =
+            {
+                new _3dPoint(),
+                new _3dPoint()
+            };
+
+            #region Read Poits From a array
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"Enter Coordinates for P{i + 1} ");
+                Console.Write("Enter X:");
+                Points[i].X = int.Parse(Console.ReadLine());
+                Console.Write("Enter Y:");
+                Points[i].Y = Convert.ToInt32(Console.ReadLine());
+                int z;
+                do
+                {
+                    Console.Write("Enter Z:");
+
+                } while (!int.TryParse(Console.ReadLine(), out z));
+                Points[i].Z = z;
+
+            }
+            Console.WriteLine(Points[0]);
+            Console.WriteLine(Points[1]);
+            #endregion
+            if (Points[0]== Points[1])
+            {
+                Console.WriteLine("Equals");
+            }
+            else
+            {
+                Console.WriteLine("ot equals");
+            }
+
         }
     }
 }
